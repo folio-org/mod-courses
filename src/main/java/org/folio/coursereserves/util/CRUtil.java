0d68275f06eadc1125217cd75063d502fcfc75c0
Adam Dickmeiss <adam@indexdata.dk>
2022-09-07 16:53:22 +0200

    Use WebClientFactory; fix socket leak MODCR-95 (#145)

--- src/main/java/org/folio/coursereserves/util/CRUtil.java
+++ src/main/java/org/folio/coursereserves/util/CRUtil.java
@@ -34,6 +34,7 @@ import static org.folio.rest.impl.CourseAPI.PROCESSING_STATUSES_TABLE;
 import static org.folio.rest.impl.CourseAPI.RESERVES_TABLE;
 import static org.folio.rest.impl.CourseAPI.TERMS_TABLE;
 
+import org.folio.okapi.common.WebClientFactory;
 import org.folio.rest.jaxrs.model.Contributor;
 import org.folio.rest.jaxrs.model.CopiedItem;
 import org.folio.rest.jaxrs.model.CopyrightStatusObject;
@@ -378,7 +379,7 @@ public class CRUtil {
       Map<String, String> okapiHeaders, String requestPath, HttpMethod method,
       Map<String, String> extraHeaders, String payload, Integer expectedCode) {
     Promise<JsonObject> promise = Promise.promise();
-    WebClient client = WebClient.create(vertx);
+    WebClient client = WebClientFactory.getWebClient(vertx);
     MultiMap headers = MultiMap.caseInsensitiveMultiMap();
     MultiMap originalHeaders = MultiMap.caseInsensitiveMultiMap();
 
